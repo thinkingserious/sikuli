9175181df0b2438b31ac454869c8af5fd164315e
Elmer Thomas <ethomas@twilio.com>
2020-11-29 14:33:12 -0800

    adusting for catalina

--- demos/empty_trash.sikuli/empty_trash.py
+++ demos/empty_trash.sikuli/empty_trash.py
@@ -2,7 +2,7 @@ from sikuli import type, mouseMove, mouseDown, click
 
 type("d", Key.ALT + Key.CMD) # show the dock, if hidden
 mouseMove("trash.png")
-mouseDown(Button.RIGHT);
+mouseDown(Button.RIGHT)
 click("empty_trash.png")
-click("empty_trash_confirm.png")
+click("empty_trash_button.png")
 mouseDown(Button.LEFT)
